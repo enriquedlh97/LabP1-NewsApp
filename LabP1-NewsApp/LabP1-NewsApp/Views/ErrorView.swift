ed12edc3f093c54ed41b8107cad1ef62211abe37
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-15 22:35:26 -0700

    Updated ErrorView with text and image

--- LabP1-NewsApp/LabP1-NewsApp/Views/ErrorView.swift
+++ LabP1-NewsApp/LabP1-NewsApp/Views/ErrorView.swift
@@ -11,7 +11,20 @@ struct ErrorView: View {
     let error: Error
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: "exclamationmark.icloud.fill")
+                .foregroundColor(.gray)
+                .font(.system(size: 50, weight: .heavy))
+                .padding(.bottom, 4)
+            Text("Something went wrong!")
+                .foregroundColor(.black)
+                .font(.system(size: 30, weight: .heavy))
+            Text(error.localizedDescription)
+                .foregroundColor(.gray)
+                .font(.system(size: 15))
+                .multilineTextAlignment(.center)
+                .padding(.vertical, 4)
+        }
     }
 }
 
