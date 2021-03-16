f2975101fec29b8fe88fab7fc84c6a35c5ce0a54
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-16 15:34:27 -0700

    Created Feed and Weather views

---
+++ LabP1-NewsApp/LabP1-NewsApp/Views/FeedView.swift
@@ -0,0 +1,19 @@
+//
+//  FeedView.swift
+//  LabP1-NewsApp
+//
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
---
+++ LabP1-NewsApp/LabP1-NewsApp/Views/WeatherView.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherView.swift
+//  LabP1-NewsApp
+//
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView()
+    }
+}
