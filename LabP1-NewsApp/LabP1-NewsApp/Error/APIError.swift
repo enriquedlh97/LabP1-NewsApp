3cac6e297d33564c665c3e8908be6c50d20c20ee
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-15 20:47:20 -0700

    Created API error enum to handles errors

--- LabP1-NewsApp/LabP1-NewsApp/Error/APIError.swift
+++ LabP1-NewsApp/LabP1-NewsApp/Error/APIError.swift
@@ -5,3 +5,12 @@
 //
 
 import Foundation
+
+enum APIError {
+    // Occurs when object fails to be called, ei. JSON structure changes, etc.
+    case decodingError
+    // Provides error code
+    case errorCode(Int)
+    // Handles any other error
+    case unknown
+}
