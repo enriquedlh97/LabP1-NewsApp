66eac3510cf8c18895408336f230002df6bdf30a
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-15 22:30:07 -0700

    Created errorView

---
+++ LabP1-NewsApp/LabP1-NewsApp/Views/ErrorView.swift
@@ -0,0 +1,19 @@
+//
+//  ErrorView.swift
+//  LabP1-NewsApp
+//
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ErrorView()
+    }
+}
