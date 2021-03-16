946dd5a34185a590ecc3a79e005093bd7aab2449
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-16 16:13:43 -0700

    Adds class to hold model data.

--- LabP1-NewsApp/LabP1-NewsApp/Model/Weather.swift
+++ LabP1-NewsApp/LabP1-NewsApp/Model/Weather.swift
@@ -5,3 +5,20 @@
 //
 
 import Foundation
+
+// The class holds the model data to be used by ViewModel to be shown in UI
+public struct Weather {
+    let city: String
+    let temperature: String
+    let description: String
+    let iconName: String
+    
+    // Take response as parameter to initialize weather model
+    init(response: APIResponse) {
+        // Get data to populate properties from above
+        city = response.name
+        temperature = "\(Int(response.main.temp))"
+        description = response.weather.first?.description ?? "" // Provides empty string if not available
+        iconName = response.weather.first?.iconName ?? "" // Provides empty string if not available
+    }
+}
