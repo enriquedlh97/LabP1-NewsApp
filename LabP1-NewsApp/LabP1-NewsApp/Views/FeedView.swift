33542b6f2cb3d43473ec87a2a5bd2ea82e5a19ae
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-16 15:38:55 -0700

    Copied everything from HomeView to FeedView and emptied the HomeView

--- LabP1-NewsApp/LabP1-NewsApp/Views/FeedView.swift
+++ LabP1-NewsApp/LabP1-NewsApp/Views/FeedView.swift
@@ -5,10 +5,49 @@
 //
 
 import SwiftUI
+import Combine
 
 struct FeedView: View {
+    
+    // Use system to open links
+    @Environment(\.openURL) var openURL
+    @StateObject var viewModel = NewsViewModelImpl(service: NewsServiceImpl())
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+        Group {
+            
+            // Observe states and display appropiate value
+            switch viewModel.state {
+            case .loading:
+                ProgressView()
+            case .failed(let error):
+                ErrorView(error: error,
+                          handler: viewModel.getArticles)
+            case .success(let articles):
+                
+                    List(articles) { item in
+                        ArticleView(article: item)
+                            .onTapGesture {
+                                load(url: item.url)
+                            }
+                    }
+                    .navigationTitle(Text("News"))
+                
+            }
+        }
+            
+        }.onAppear(perform: viewModel.getArticles) // Automatically calls for articles fucntion
+    
+    }
+    
+    func load(url: String?) {
+        
+        guard let link = url,
+              let url = URL(string: link) else { return }
+        
+        openURL(url)
+        
     }
 }
 
