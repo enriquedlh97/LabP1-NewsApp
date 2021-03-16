d7a851c8d78f9b3519466cdecfe20f670b8155eb
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-15 20:56:16 -0700

    Created base request protocol

--- LabP1-NewsApp/LabP1-NewsApp/Service/NewsService.swift
+++ LabP1-NewsApp/LabP1-NewsApp/Service/NewsService.swift
@@ -5,3 +5,11 @@
 //
 
 import Foundation
+import Combine
+
+// Anypublisher allows to subscribe to the event we want to listen to.
+// NewsResponse: Success
+// APIError: Failure
+protocol  NewsService {
+    func request(from endpoint: NewsAPI) -> AnyPublisher<NewsResponse, APIError>
+}
