9b088587d531508b64dfd8a9abf35f797c6fe84e
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-15 21:19:34 -0700

    Created 3 states supported by the app

--- LabP1-NewsApp/LabP1-NewsApp/States/ResultState.swift
+++ LabP1-NewsApp/LabP1-NewsApp/States/ResultState.swift
@@ -5,3 +5,10 @@
 //
 
 import Foundation
+
+// Three states included to change the view depending ont he state
+enum ResultState {
+    case loading
+    case success(content: [Article])
+    case failed(error: Error)
+}
