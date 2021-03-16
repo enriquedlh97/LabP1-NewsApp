157fec2935cda00ecf8c04ff3c1d3e128945e32f
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-15 22:14:40 -0700

    Updated ArticleView

--- LabP1-NewsApp/LabP1-NewsApp/Views/ArticleView.swift
+++ LabP1-NewsApp/LabP1-NewsApp/Views/ArticleView.swift
@@ -14,6 +14,31 @@ struct ArticleView: View {
     var body: some View {
         HStack {
             
+            if let imgUrl = article.image,
+               let url = URL(string: imgUrl) {
+                
+                URLImage(url: url,
+                         options: URLImageOptions(
+                            identifier: article.id.uuidString,
+                            cachePolicy:
+                                .returnCacheElseLoad(cacheDelay: nil, downloadDelay: 0.25)
+                         ),
+                         failuer: { error, _ in
+                            Image(systemName: "photo.fill")
+                                .foregroundColor(.white)
+                                .background(Color.gray)
+                                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                         },
+                         content: { image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                         })
+                // Build asynchronous image object
+            } else {
+                
+            }
+            
         }
     }
 }
