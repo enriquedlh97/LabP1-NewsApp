4eb59ba0b79720e3d3f088d60114683cb8a2655e
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-15 20:19:12 -0700

    Created HomeView and replaced COntentView file with it

---
+++ LabP1-NewsApp/LabP1-NewsApp/LabP1_NewsAppApp.swift
@@ -0,0 +1,16 @@
+//
+//  LabP1_NewsAppApp.swift
+//  LabP1-NewsApp
+//
+//
+
+import SwiftUI
+
+@main
+struct LabP1_NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
