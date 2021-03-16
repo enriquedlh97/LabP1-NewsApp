531e6edf4a4c0d5603e537cda750e86403d5931b
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-15 22:05:20 -0700

    Created dummy data in Article struct

--- LabP1-NewsApp/LabP1-NewsApp/Model/NewsResponse.swift
+++ LabP1-NewsApp/LabP1-NewsApp/Model/NewsResponse.swift
@@ -25,3 +25,19 @@ struct Article: Codable, Identifiable {
         case image, date
     }
 }
+
+
+// Creates dummy data for article
+extension Article {
+    
+    static var dummyData: Article {
+        .init(author: "Joseph Wulfson",
+              url: "https://www.foxnews.com/media/new-york-magazine-jonathan-chait-gina-carano-disney-hollywood-blacklist",
+              source: "Fox News",
+              title: "Disney's Gina Carano firing denounced as 'Hollywod Blacklist' against conservatives - Fox News",
+              articleDescription: "Conservative actress Gina Carano found rare support among liberals in the mainstream media following her firing from \"The Mandalorian\" over social media posts and being dropped by her agency UTA.",
+              image: "https://static.foxnews.com/foxnews.com/content/uploads/2021/02/Gina-Carano.jpg",
+              date: Date())
+    }
+    
+}
--- LabP1-NewsApp/LabP1-NewsApp/Views/ArticleView.swift
+++ LabP1-NewsApp/LabP1-NewsApp/Views/ArticleView.swift
@@ -8,8 +8,13 @@ import SwiftUI
 import URLImage
 
 struct ArticleView: View {
+    
+    let article: Article
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            
+        }
     }
 }
 
