7f2a40fca241844d2655ec52abf6c61b016b7f37
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-15 20:30:50 -0700

    Created NewsResponse and Article structs

--- LabP1-NewsApp/LabP1-NewsApp/Model/NewsResponse.swift
+++ LabP1-NewsApp/LabP1-NewsApp/Model/NewsResponse.swift
@@ -5,3 +5,23 @@
 //
 
 import Foundation
+
+struct NewsResponse: Codable {
+    let articles: [Article]
+}
+
+
+struct Article: Codable, Identifiable {
+    let id = UUID()
+    let author: String?
+    let url: String
+    let source, title, articleDescription: String
+    let image: String
+    let date: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case author, url, source, title
+        case articleDescription = "description"
+        case image, date
+    }
+}
