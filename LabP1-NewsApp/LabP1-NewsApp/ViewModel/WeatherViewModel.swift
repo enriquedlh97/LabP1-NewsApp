b1cbb1718e58b8bd06a8b6b510a69f09e361bf23
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-16 16:56:43 -0700

    Add properties updates

--- LabP1-NewsApp/LabP1-NewsApp/ViewModel/WeatherViewModel.swift
+++ LabP1-NewsApp/LabP1-NewsApp/ViewModel/WeatherViewModel.swift
@@ -22,4 +22,26 @@ public class WeatherViewModel: ObservableObject {
     @Published var temperature: String = "--"
     @Published var weatherDescription: String = "--"
     @Published var weatherIcon: String = defaultIcon
+    
+    // Used to get the weather data
+    public let weatherService: WeatherService
+    
+    // Get weatherService when the viewModel is initialized
+    public init(weatherService: WeatherService) {
+        // asign the weatherService property
+        self.weatherService = weatherService
+    }
+     
+    // Calls the weatherService method above to load data and then update @Published properties for the view to display
+    public func refresh() {
+        weatherService.loadWeatherData { weather in
+            // Update properties. Use DispatchQueue since they update the ui and they have to be updated in the mai Queue
+            DispatchQueue.main.async {
+                self.cityName = weather.city
+                self.temperature = "\(weather.temperature)°C"
+                self.weatherDescription = weather.description.capitalized
+                self.weatherIcon = iconMap[weather.iconName] ?? defaultIcon
+            }
+        }
+    }
 }
