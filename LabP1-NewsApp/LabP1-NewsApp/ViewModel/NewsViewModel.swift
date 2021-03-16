e8dd382e646f298d029cd1c739c5cf81e3d9a56b
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-15 21:28:08 -0700

    Added dependency injectuion to NewsViewModel class

--- LabP1-NewsApp/LabP1-NewsApp/ViewModel/NewsViewModel.swift
+++ LabP1-NewsApp/LabP1-NewsApp/ViewModel/NewsViewModel.swift
@@ -5,3 +5,26 @@
 //
 
 import Foundation
+import Combine
+
+protocol  NewsViewModel {
+    func getArticles()
+}
+
+
+// Class as observable object that implements the above protocol
+// Observable object is used to observe the changes in the class
+class NewsViewModelImpl: ObservableObject, NewsViewModel {
+    
+    private let service: NewsService
+    
+    // Injected news service into the class (basic start of the request).
+    // Essentially a Dependency injection
+    init(service: NewsService) {
+        self.service = service
+    }
+    
+    func getArticles() {
+        <#code#>
+    }
+}
