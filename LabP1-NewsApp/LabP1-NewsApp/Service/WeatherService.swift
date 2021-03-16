f5245ab18c0a66a060d0e34a44d48efe0edaa74a
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-16 15:59:49 -0700

    Added 3 structs to WeatherService. These represent the JSON response

--- LabP1-NewsApp/LabP1-NewsApp/Service/WeatherService.swift
+++ LabP1-NewsApp/LabP1-NewsApp/Service/WeatherService.swift
@@ -5,3 +5,30 @@
 //
 
 import Foundation
+
+//These 3 structures represent the JSON response from the API
+
+// Struct to represent the complete reponse of the API
+struct APIResponse {
+    // Saves gthe name of the city we are getting the weather for
+    let name: String
+    let main: APIMain
+    let weather: [APIWeather]
+}
+
+// Used to get the temperature
+struct APIMain: Decodable {
+    let temp: Double
+}
+
+// Used to get the description of weather and the icon to be displayed
+struct APIWeather: Decodable {
+    let description: String
+    let iconName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case description
+        // Use the key main to get the value of the iconName from the JSON
+        case iconName = "main"
+    }
+}
