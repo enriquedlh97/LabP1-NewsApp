add176b6472f30bd645bff2e0bdd095afc12f532
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-15 21:32:14 -0700

    Adds collections of cancellables to hold requests in memory

--- LabP1-NewsApp/LabP1-NewsApp/ViewModel/NewsViewModel.swift
+++ LabP1-NewsApp/LabP1-NewsApp/ViewModel/NewsViewModel.swift
@@ -18,9 +18,11 @@ class NewsViewModelImpl: ObservableObject, NewsViewModel {
     
     private let service: NewsService
     
-    // (set) allows the variable to be protected but still accesable within the class
+    // (set) allows the variable to be protected but still accesable outside the class
     // This array holds the news articles
     private(set) var articles = [Article]()
+    // Will hold the requestsin memory to avoid getting disposed of the service when calling it
+    private var cancellables = Set<AnyCancellable>()
     
     // Injected news service into the class (basic start of the request).
     // Essentially a Dependency injection
