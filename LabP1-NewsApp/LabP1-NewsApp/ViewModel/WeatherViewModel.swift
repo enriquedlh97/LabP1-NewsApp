e84dd34ea969798d7311ebaeb0de5b4eb83ef305
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-16 16:50:08 -0700

    Added iconMap and default icon in WeatherviewModel

--- LabP1-NewsApp/LabP1-NewsApp/ViewModel/WeatherViewModel.swift
+++ LabP1-NewsApp/LabP1-NewsApp/ViewModel/WeatherViewModel.swift
@@ -5,3 +5,21 @@
 //
 
 import Foundation
+
+private let defaultIcon = " ❓ "
+private let iconMap = [
+    "Drizzle" : "🌧",
+    "Thunderstorm" : "⛈",
+    "Rain": "🌧",
+    "Snow": "🌨",
+    "Clear": "☀️",
+    "Clouds": "☁️",
+]
+
+// Gets data from service and converts model to the info that the view needs to be displayed in the app
+public class WeatherViewModel: ObservableObject {
+    @Published var cityName: String = "City Name"
+    @Published var temperature: String = "--"
+    @Published var weatherDescription: String = "--"
+    @Published var weatherIcon: String = defaultIcon
+}
