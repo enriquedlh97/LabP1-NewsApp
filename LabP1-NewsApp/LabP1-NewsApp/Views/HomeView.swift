9d1858ef55534537db964e72cf647fc770674ce7
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-16 17:53:06 -0700

    Update

--- LabP1-NewsApp/LabP1-NewsApp/Views/HomeView.swift
+++ LabP1-NewsApp/LabP1-NewsApp/Views/HomeView.swift
@@ -32,7 +32,7 @@ struct HomeView: View {
                             }
                     }
                     .navigationTitle(Text("News"))
-                
+                    
             }
         }
             
